3e9f950ca81d46657f099e5fbebe252a83e9c0f6
Janosch Püschel <janosch.pueschel@gmail.com>
2023-03-16 10:54:58 +0100

    add function to create  book object by user input

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+function createNewBook() {
+  let title = prompt("Book-Title:");
+  let author = prompt("Author:");
+  let pages = prompt("Pages:");
+  let read = prompt("Read:");
+  let newBook = new Book(title, author, pages, read);
+}
