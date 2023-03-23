f8beb09db88819a63df3f81c60a46896a4d8beef
Janosch Püschel <janosch.pueschel@gmail.com>
2023-03-23 23:39:42 +0100

    add cancel button to close form without action

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function displayBooks() {
         bookCard.innerHTML += `<p>${books[key]}</p>`;
       }
     }
-   
+
     bookCard.innerHTML += `<button class="delete-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg></button>`;
 
     let readStatus = document.getElementById("read-status");
@@ -104,7 +104,9 @@ function updateReadStatus() {
   for (let i = 0; i < readStatusCheckbox.length; i++) {
     readStatusCheckbox[i].addEventListener("click", () => {
       let libraryIndex =
-        readStatusCheckbox[i].parentNode.parentNode.getAttribute("data-library-index");
+        readStatusCheckbox[i].parentNode.parentNode.getAttribute(
+          "data-library-index"
+        );
       if (myLibrary[libraryIndex]["read"] === "read") {
         myLibrary[libraryIndex]["read"] = "not read";
       } else if (myLibrary[libraryIndex]["read"] === "not read") {
@@ -115,10 +117,5 @@ function updateReadStatus() {
   }
 }
 
-
-/* 
-- Overlay
-- input fields to be required
-- button-hover
-
-*/
+const cancelBtn = document.getElementById("cancel-btn");
+cancelBtn.addEventListener("click", closeForm);
